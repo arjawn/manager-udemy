45599997672cb71bd469592900283a6be34857f2
Arjun Mahadevan <mahadevanarjun21@gmail.com>
2019-05-11 14:31:50 -0700

    Login form complete

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
+import ReduxThunk from 'redux-thunk';
+import reducers from './reducers';
+import LoginForm from './components/LoginForm';
+
+class App extends Component {
+  componentWillMount() {
+    const firebaseConfig = {
+      apiKey: ' AIzaSyAh2wfmtpbQDvV-kh5HKXBQHGi6UFKxVYQ',
+      authDomain: 'manager-ede25.firebaseapp.com',
+      databaseURL: 'https://manager-ede25.firebaseio.com',
+      projectId: 'manager-ede25',
+      storageBucket: 'manager-ede25.appspot.com',
+      messagingSenderId: '800843425273',
+      appId: '1:800843425273:web:46111e361f8f8c10'
+    };
+
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
+
+  render() {
+    const store = createStore(reducers, {}, applyMiddleware(ReduxThunk));
+
+    return (
+      <Provider store={store}>
+        <LoginForm />
+      </Provider>
+    );
+  }
+}
+
+export default App;
